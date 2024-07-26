c4cc9ae34156fa53d5d233e5cc4f8e265f9ad21d
linpengteng <china.linpengteng@gmail.com>
2024-07-26 10:31:32 +0800

    chore: add .nuxtignore file

--- server/plugins/migrations.ts
+++ server/plugins/migrations.ts
@@ -1,17 +1,17 @@
-// import { consola } from 'consola'
-// import { migrate } from 'drizzle-orm/d1/migrator'
+import { consola } from 'consola'
+import { migrate } from 'drizzle-orm/d1/migrator'
 
 export default defineNitroPlugin(async () => {
   if (!import.meta.dev) return
 
-  // enable when Only using local sql
-  // onHubReady(async () => {
-  //   await migrate(useDB(), { migrationsFolder: 'server/database/migrations' })
-  //     .then(() => {
-  //       consola.success('Database migrations done')
-  //     })
-  //     .catch((err) => {
-  //       consola.error('Database migrations failed', err)
-  //     })
-  // })
+  // enable when Only using local sql (note: only need to be used once)
+  onHubReady(async () => {
+    await migrate(useDB(), { migrationsFolder: 'server/database/migrations' })
+      .then(() => {
+        consola.success('Database migrations done')
+      })
+      .catch((err) => {
+        consola.error('Database migrations failed', err)
+      })
+  })
 })

ca58aea841f7b610be61fbf8b569f1566964e089
linpengteng <china.linpengteng@gmail.com>
2024-07-26 09:19:35 +0800

    test: run some scripts
    1. npx nuxthub login
    2. node -e "console.log(require('crypto').randomBytes(32).toString('hex'))"
    3. npx wrangler d1 execute nuxt-todos --remote --file=./server/database/schema.ts

--- server/plugins/migrations.ts
+++ server/plugins/migrations.ts
@@ -1,16 +1,17 @@
-import { consola } from 'consola'
-import { migrate } from 'drizzle-orm/d1/migrator'
+// import { consola } from 'consola'
+// import { migrate } from 'drizzle-orm/d1/migrator'
 
 export default defineNitroPlugin(async () => {
   if (!import.meta.dev) return
 
-  onHubReady(async () => {
-    await migrate(useDB(), { migrationsFolder: 'server/database/migrations' })
-      .then(() => {
-        consola.success('Database migrations done')
-      })
-      .catch((err) => {
-        consola.error('Database migrations failed', err)
-      })
-  })
+  // enable when Only using local sql
+  // onHubReady(async () => {
+  //   await migrate(useDB(), { migrationsFolder: 'server/database/migrations' })
+  //     .then(() => {
+  //       consola.success('Database migrations done')
+  //     })
+  //     .catch((err) => {
+  //       consola.error('Database migrations failed', err)
+  //     })
+  // })
 })
